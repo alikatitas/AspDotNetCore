27a2a10accb47a238e0ec5ad5d29ed9559d3bb73
alikatitas <alikatitas@e-mail.com.tr>
2025-09-14 04:37:20 -0700

    DI and IOC

---
+++ Store/StoreApp/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+using StoreApp.Models;
+
+namespace StoreApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Dependency Injection
+        private readonly RepositoryContext _context;
+        public ProductController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // Send to View Page
+            var model = _context.Products.ToList();
+            return View(model);
+        }
+
+        public IActionResult Get(int id)
+        {
+            //Inversion of Control
+            Product pr = _context.Products.First(p=> p.ProductId.Equals(id));
+            return View(pr);
+        }
+
+    }
+}
