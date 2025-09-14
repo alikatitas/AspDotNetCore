d492b47bf418b04404ca43d33336499734299371
alikatitas <alikatitas@e-mail.com.tr>
2025-09-14 03:36:47 -0700

    enter values to sqlite3

--- Store/StoreApp/Models/RepositoryContext.cs
+++ Store/StoreApp/Models/RepositoryContext.cs
@@ -5,9 +5,22 @@ namespace StoreApp.Models
     public class RepositoryContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
-        public RepositoryContext(DbContextOptions<RepositoryContext> options)
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
         {
-            
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+            .HasData(
+                new Product(){ProductId=1,ProductName="Computer",Price=2_000},
+                new Product(){ProductId=2,ProductName="Keyboard",Price=500},
+                new Product(){ProductId=3,ProductName="Mouse",Price=250},
+                new Product(){ProductId=4,ProductName="TV",Price=3_700}
+                
+            );
         }
     }
 }
--- Store/StoreApp/obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
+++ Store/StoreApp/obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58f74f98f461b10a2a44eeab7715ed884904c040")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03cae686a8203cc96c43ffd8e5999e2acf5ef414")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
